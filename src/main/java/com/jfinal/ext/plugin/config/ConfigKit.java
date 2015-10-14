e6550e7619dfd525fa30850e25de5be534284ebd
moyu <moyu@9beike.com>
2015-10-14 14:45:17 +0800

    支持直接读取boo型参数

--- src/main/java/com/jfinal/ext/plugin/config/ConfigKit.java
+++ src/main/java/com/jfinal/ext/plugin/config/ConfigKit.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (c) 2011-2013, kidzhou 周磊 (zhouleib1412@gmail.com)
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -149,9 +149,15 @@ public class ConfigKit {
         return getInt(key, 0);
     }
 
-    public static void set(String key, String val) {
-        // TODO
-        throw new RuntimeException("I do not know how to do it now..");
+    public Boolean getBoolean(String key) {
+        return getBoolean(key, null);
     }
 
+    public Boolean getBoolean(String key, Boolean defaultValue) {
+        String val = getStr(key).trim();
+        if ("".equals(val)) {
+            return defaultValue;
+        }
+        return (val != null) ? Boolean.parseBoolean(val) : defaultValue;
+    }
 }
